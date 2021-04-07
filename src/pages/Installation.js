16573d83d7553080193e4d454404d5db88ca5a0f
Jimmy <jimmyjunior406@gmail.com>
2021-04-06 22:31:21 -0400

    installation page is set up

--- src/pages/Installation.js
+++ src/pages/Installation.js
@@ -2,9 +2,12 @@ import React, { useState } from 'react';
 
 export default function Installation(props) {
 	return (
-		<div class="flex justify-center mt-8 ">
-			<div class="border-solid rounded-md border-4 border-gray-400">
+		<div class="flex items-stretch mt-8">
+			<div class="border-solid rounded-md border-4 border-gray-400 m-4">
 				<img src="https://i.imgur.com/JpPhSbnl.png" />
+			</div>
+			<div class="font-sans text-base self-center m-4">
+				This is were the installion information is going to go
 			</div>
 		</div>
 	);
