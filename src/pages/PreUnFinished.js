159cc1974639c49e573921d531731eaca2a189cc
Jimmy <jimmyjunior406@gmail.com>
2021-04-07 22:46:27 -0400

    all pages similair

--- src/pages/PreUnFinished.js
+++ src/pages/PreUnFinished.js
@@ -2,12 +2,12 @@ import React, { useState } from 'react';
 
 export default function PreUnfinished(props) {
 	return (
-		<div class="flex items-stretch mt-8">
-			<div class="border-solid rounded-md border-4 border-gray-400 m-4">
+		<div class="container flex justify-center">
+			<div class="relative top-28 border-solid rounded-md border-4 border-gray-400 -ml-50px w-5/12 h-3/4">
 				<img src="https://i.imgur.com/rLqdFckl.png" />
 			</div>
 			<div class="font-sans text-base self-center m-4">
-				This is were the Prefinished and Unfinished information is going to go
+				This is were the installion information is going to go
 			</div>
 		</div>
 	);
--- src/pages/Refinish.js
+++ src/pages/Refinish.js
@@ -2,9 +2,12 @@ import React, { useState } from 'react';
 
 export default function Contact(props) {
 	return (
-		<div class="flex justify-center mt-8 ">
-			<div class="border-solid rounded-md border-4 border-gray-400">
+		<div class="container flex justify-center">
+			<div class="relative top-28 border-solid rounded-md border-4 border-gray-400 -ml-50px w-5/12 h-3/4">
 				<img src="https://i.imgur.com/jIIWjlcl.png" />
+			</div>
+			<div class="font-sans text-base self-center m-4">
+				This is were the installion information is going to go
 			</div>
 		</div>
 	);
